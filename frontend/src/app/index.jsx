43107d1fef287a3ebd0ba3702b50ace32fede603
Thtzx <huetzentom@gmail.com>
2026-03-12 22:56:39 +0100

    added home button to index

--- frontend/src/app/index.jsx
+++ frontend/src/app/index.jsx
@@ -1,13 +1,14 @@
+import AppButton from "@/components/ui/AppButton";
 import AppText from "@/components/ui/AppText";
+import { router } from "expo-router";
 import { View } from "react-native";
 
 export default function Home() {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <AppText type="body">🎉 Expo Router works 🎉</AppText>
-      <AppText bold type="body">
-        Go to /home
-      </AppText>
+      <AppText type="h1">Hello there</AppText>
+      <AppText type="body">Welcome to LifePoints</AppText>
+      <AppButton title={'go to homepage'} onPress={() => router.replace('/home')} style={{ marginTop: 20 }} />
     </View>
   );
 }
