904e56b373b7500ec11515674f5f4359684613e2
Tom Hützen <Tom.Huetzen@dts.de>
2026-05-26 15:55:55 +0200

    changed onboardingGuide quest order

--- frontend/src/constants/OnboardingGuideSteps.js
+++ frontend/src/constants/OnboardingGuideSteps.js
@@ -1,30 +1,30 @@
 export const onboardingGuideSteps = [
   {
     id: "1",
-    title: "Add profile picture",
-    reward: 10,
-    icon: "camera",
-    route: '/setting/edit-profile'
-  },
-  {
-    id: "2",
-    title: "Add your first friend",
-    reward: 20,
-    icon: "profile",
-    route: '/user/Velo_Mind'
-  },
-  {
-    id: "3",
     title: "Log your first task",
     reward: 50,
     icon: "heart",
     route: '/tasks'
   },
   {
-    id: "4",
+    id: "2",
     title: "Add a description",
     reward: 15,
     icon: "pencil",
     route: '/setting/edit-profile'
+  },
+  {
+    id: "3",
+    title: "Add profile picture",
+    reward: 10,
+    icon: "camera",
+    route: '/setting/edit-profile'
+  },
+  {
+    id: "4",
+    title: "Add your first friend",
+    reward: 20,
+    icon: "profile",
+    route: '/home'
   },
 ];
