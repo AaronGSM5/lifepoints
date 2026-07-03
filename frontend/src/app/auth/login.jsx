4e435c4b3216447c6a9f7b77104c59d90653e368
Aaron Gaßmann <aaron.gassmann@dts.de>
2026-07-03 15:55:25 +0200

    remove double imports

--- frontend/src/app/auth/login.jsx
+++ frontend/src/app/auth/login.jsx
@@ -5,16 +5,12 @@ import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 import AuthFooter from "@/components/auth/AuthFooter";
 import AuthHeader from "@/components/auth/AuthHeader";
-import PasswordInput from "@/components/auth/PasswordInput";
 import ScreenWrapper from "@/components/layout/ScreenWrapper";
 import AppButton from "@/components/ui/AppButton";
 import AppInput from "@/components/ui/AppInput";
 
 // Import your Appwrite account instance
 import { account } from "@/lib/appwrite";
-import PasswordInput from "@/components/auth/PasswordInput";
-import AuthHeader from "@/components/auth/AuthHeader";
-import AuthFooter from "@/components/auth/AuthFooter";
 import BaseCard from "@/components/ui/BaseCard";
 import { Spacing } from "@/constants/Spacing";
 import { useAppTheme } from "@/hooks/useAppTheme";
