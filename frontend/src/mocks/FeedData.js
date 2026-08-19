9e6ab08f4e183ba3b4036ee626b42d0ad8f30d4f
Thtzx <huetzentom@gmail.com>
2026-08-19 15:44:52 +0200

    added mockFeedItems

--- frontend/src/mocks/FeedData.js
+++ frontend/src/mocks/FeedData.js
@@ -15,21 +15,57 @@ export const mockFeedItems = [
     username: 'Velo_Mind',
     badge: 'badge_elite',
     description: 'You vs You',
-    image: require("@/../public/assets/posts/sportSelfie2.jpg"),
+    image: require("@/../public/assets/posts/miniWorkout.webp"),
     avatar: 'https://i.pravatar.cc/150?u=Velo_Mind'
   },
   {
     id: 3,
+    type: 'image',
+    username: 'Velo_Mind',
+    badge: 'badge_elite',
+    description: 'Plants are so beautifull :)',
+    image: require("@/../public/assets/posts/plantSeed.webp"),
+    avatar: 'https://i.pravatar.cc/150?u=Velo_Mind'
+  },
+  {
+    id: 4,
+    type: 'image',
+    username: 'Velo_Mind',
+    badge: 'badge_elite',
+    description: 'You vs You',
+    image: require("@/../public/assets/posts/sportSelfie2.jpg"),
+    avatar: 'https://i.pravatar.cc/150?u=Velo_Mind'
+  },
+  {
+    id: 5,
     type: 'video',
     username: 'LifePoints_Dev',
     badge: 'badge_elite',
-    description: 'Erster Videowdasd-Feed Test! 🚀🎥',
+    description: 'Zweiter Video-Feed Test! 🚀🎥',
     videoUrl: require("@/../public/assets/posts/testVideo2.mp4"),
     thumbnail: require("@/../public/assets/posts/sportSelfie1.jpeg"),
     avatar: 'https://i.pravatar.cc/150?u=LifePoints_Dev'
   },
   {
-    id: 4,
+    id: 6,
+    type: 'image',
+    username: 'PixelPionier',
+    badge: 'badge_rookie',
+    description: 'Feeling soo relaxed now. Recommend it!',
+    image: require("@/../public/assets/posts/stretchMoment.webp"),
+    avatar: 'https://i.pravatar.cc/150?u=PixelPionier'
+  },
+  {
+    id: 7,
+    type: 'image',
+    username: 'PixelPionier',
+    badge: 'badge_rookie',
+    description: 'Happy mothers day',
+    image: require("@/../public/assets/posts/digitalThanks.webp"),
+    avatar: 'https://i.pravatar.cc/150?u=PixelPionier'
+  },
+  {
+    id: 8,
     type: 'image',
     username: 'PixelPionier',
     badge: 'badge_rookie',
@@ -38,7 +74,25 @@ export const mockFeedItems = [
     avatar: 'https://i.pravatar.cc/150?u=PixelPionier'
   },
   {
-    id: 5,
+    id: 9,
+    type: 'image',
+    username: 'Nox_Echo',
+    badge: 'badge_grinder',
+    description: 'Always think about the ones who have less',
+    image: require("@/../public/assets/posts/spreadJoy.webp"),
+    avatar: 'https://i.pravatar.cc/150?u=Nox_Echo'
+  },
+  {
+    id: 10,
+    type: 'image',
+    username: 'Nox_Echo',
+    badge: 'badge_grinder',
+    description: 'Take care of your City!',
+    image: require("@/../public/assets/posts/natureCleaning.webp"),
+    avatar: 'https://i.pravatar.cc/150?u=Nox_Echo'
+  },
+  {
+    id: 11,
     type: 'image',
     username: 'Nox_Echo',
     badge: 'badge_grinder',
@@ -47,7 +101,7 @@ export const mockFeedItems = [
     avatar: 'https://i.pravatar.cc/150?u=Nox_Echo'
   },
   {
-    id: 6,
+    id: 12,
     type: 'image',
     username: 'Zest_Runner',
     badge: null,
